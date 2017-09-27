44f8f12ae108d789cfaedc8a8933133a42bf4ea1
huynh-primelabo <sy.huynh@primelabo.com.vn>
2017-09-27 14:33:20 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "jquery",
+    "xo/esnext"
+  ],
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jquery": true
+  },
+  "rules": {
+    "no-console": "error",
+    "indent": ["error",4],
+  }
+};
